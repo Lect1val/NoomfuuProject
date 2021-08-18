5ee89865c44cca2964a16fd4ba1f5b718f5640f6
Poi <o.onepiece9@gmail.com>
2021-08-18 15:49:10 +0700

    [DELETE] Delete LIFF Folder and Route LIFF

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-#Noomfuu Chatbot 
-#Import Library
+# Noomfuu Chatbot
+# Import Library
 import json
 import os
 from flask import Flask
@@ -8,80 +8,85 @@ from flask import make_response
 
 from sentimentmodel import useSentiment
 
-#----Firebase----
+# ----Firebase----
 from random import randint
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-cred = credentials.Certificate("noomfuuproject-f3b69-firebase-adminsdk-mpbf0-0db0cba4ee.json")
+cred = credentials.Certificate(
+    "noomfuuproject-f3b69-firebase-adminsdk-mpbf0-0db0cba4ee.json")
 firebase_admin.initialize_app(cred)
-#-------------------------------------
+# -------------------------------------
 
 db = firestore.client()
 
 # Flask
 app = Flask(__name__)
-@app.route('/', methods=['POST']) #Using post as a method
+
+
+@app.route('/', methods=['POST'])  # Using post as a method
 # @app.route('/')
-
-
 def MainFunction():
 
-    #Getting intent from Dailogflow
+    # Getting intent from Dailogflow
     data_from_dailogflow_raw = request.get_json(silent=True, force=True)
     #user_text = data_from_dailogflow_raw["queryResult"]
 
-    #Call generating_answer function to classify the question
+    # Call generating_answer function to classify the question
     answer_from_bot = generating_answer(data_from_dailogflow_raw)
-    
-    #Make a respond back to Dailogflow
+
+    # Make a respond back to Dailogflow
     r = make_response(answer_from_bot)
-    r.headers['Content-Type'] = 'application/json' #Setting Content Type
+    r.headers['Content-Type'] = 'application/json'  # Setting Content Type
 
     return r
-    #return user_text
+    # return user_text
+
 
 def generating_answer(data_from_dailogflow):
-    
-    #Print intent that recived from dialogflow.
-    print(json.dumps(data_from_dailogflow, indent=4 ,ensure_ascii=False))
 
-    #Getting intent name form intent that recived from dialogflow.
-    intent_group_question_str = data_from_dailogflow["queryResult"]["intent"]["displayName"] 
+    # Print intent that recived from dialogflow.
+    print(json.dumps(data_from_dailogflow, indent=4, ensure_ascii=False))
 
-    #Select function for answering question
+    # Getting intent name form intent that recived from dialogflow.
+    intent_group_question_str = data_from_dailogflow["queryResult"]["intent"]["displayName"]
+
+    # Select function for answering question
     if intent_group_question_str == "NegativeEmotion - yes - want - problem":
         answer_str = NegativeEmotion_problem(data_from_dailogflow)
-    else: answer_str = "นุ่มฟูไม่เข้าใจ"
+    else:
+        answer_str = "นุ่มฟูไม่เข้าใจ"
 
-    #Build answer dict 
+    # Build answer dict
     answer_from_bot = {"fulfillmentText": answer_str}
-    
-    #Convert dict to JSON   
-    answer_from_bot = json.dumps(answer_from_bot, indent=4) 
-    
+
+    # Convert dict to JSON
+    answer_from_bot = json.dumps(answer_from_bot, indent=4)
+
     return answer_from_bot
+
 
 def NegativeEmotion_problem(input_from_user):
     user_problem = input_from_user["queryResult"]["queryText"]
     userID = input_from_user["originalDetectIntentRequest"]["payload"]["data"]["source"]["userId"]
     db.collection('User').document(f'{userID}/message/problem').set({
-        u'content' : {user_problem}
+        u'content': {user_problem}
     })
     function_answer = "เราเข้าใจความรู้สึกของเธอนะ ไม่เป็นไรนะ ลองถอยออกมาจากสิ่งที่ทำให้เครียดบ้าง เป็นกำลังใจให้เสมอนะ"
     return function_answer
+
 
 def use_sentiment(word):
     answer_function = useSentiment(word)
     return answer_function
 
-#Flask
+# Flask
 
-@app.route('/assessment')
-def index():
-    return render_template('LIFF/assessment2Q.html')
+# @app.route('/assessment')
+# def index():
+#     return render_template('LIFF/assessment2Q.html')
 
-    
+
 if __name__ == '__main__':
     app.run()
     """
