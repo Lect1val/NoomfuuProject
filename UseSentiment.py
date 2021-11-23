6e90893980a587c59c8a2114793dd47e5f1768ce
Pete <thannakorn.lectival@mail.kmutt.ac.th>
2021-11-23 21:22:32 +0700

    [EDIT] Update Sentiment analyze

--- UseSentiment.py
+++ UseSentiment.py
@@ -28,16 +28,16 @@ training_data = list(zip(listpos,pos1)) + list(zip(listneg,neg1))
 
 vocabulary = set(chain(*[word_tokenize(i[0].lower()) for i in training_data]))
 
+print('start')
+
 def useSentiment(Message):
   while True:
       # test_sentence = input('\nข้อความ : ')
       featurized_test_sentence = {i:(i in word_tokenize(Message.lower())) for i in vocabulary}
-      emotion = classifier.classify(featurized_test_sentence)
-      print("test_sentiment:",Message)
-      print("tag:",emotion) # ใช้โมเดลที่ train ประมวลผล
-      return emotion
+      print("test_sent:",Message)
+      print("tag:",classifier.classify(featurized_test_sentence)) # ใช้โมเดลที่ train ประมวลผล
       break
 
-# while True:
-#   Message = input('\nข้อความ : ')
-#   useSentiment(Message)
+while True:
+  Message = input('\nข้อความ : ')
+  useSentiment(Message)
